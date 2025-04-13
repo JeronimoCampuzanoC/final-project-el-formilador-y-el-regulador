c7a2f5c8da037f29ab34e670bdc0abc51e80ddda
Jeronimo Campuzano Castaño <jcampuzanc@eafit.edu.co>
2025-04-12 22:54:32 -0500

    Basic layout for the project

---
+++ build/CMakeFiles/final_project_el_formilador_y_el_regulador.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/final_project_el_formilador_y_el_regulador.dir/src/main.cpp.o"
+  "CMakeFiles/final_project_el_formilador_y_el_regulador.dir/src/main.cpp.o.d"
+  "final_project_el_formilador_y_el_regulador"
+  "final_project_el_formilador_y_el_regulador.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/final_project_el_formilador_y_el_regulador.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/final_project_el_formilador_y_el_regulador.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for final_project_el_formilador_y_el_regulador.
+# This may be replaced when dependencies are built.
